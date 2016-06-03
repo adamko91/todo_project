84ee959af5c86361f7fdc9ac8db4fe734eccdd36
adamko91 <kozub66@poczta.fm>
2016-06-03 20:47:12 +0000

    dodanie firebase

--- app/index.js
+++ app/index.js
@@ -5,6 +5,9 @@ import uirouter from 'angular-ui-router';
 
 import routing from './config';
 
+import 'firebase';
+import angularFire from 'angularfire';
+
 import log from './logowanie';
 
 const ngModule = angular
